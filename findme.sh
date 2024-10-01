647dfad1fb69e235f7c491b5b91b2ab411b46d2b
Adambekkali <105216498+Adambekkali@users.noreply.github.com>
2024-10-01 15:37:07 +0200

    Update findme.sh
    affichage ajouté

--- findme.sh
+++ findme.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-find /home -name "FindMe.txt" -type f -exec rm -f {} +
+
+echo "$(date +"%b %d %T") $(hostname) findme[$$]: Searching for >
+
+files=$(find /home -name "FindMe.txt" -type f)
+
+if [ -z "$files" ]; then
+    echo "$(date +"%b %d %T") $(hostname) findme[$$]: Can’t find>
+else
+    echo "$files" | while read -r file; do
+        echo "$(date +"%b %d %T") $(hostname) findme[$$]: Found >
+        rm -f "$file"
+        echo "$(date +"%b %d %T") $(hostname) findme[$$]: Delete>
+    done
+    echo "$(date +"%b %d %T") $(hostname) findme[$$]: Restarting>
+fi
